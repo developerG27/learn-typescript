853aba72b7c5db4eebcad812b48f03846be46f1b
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-14 15:00:49 +0200

    decorator for clas

--- decorator.ts
+++ decorator.ts
@@ -1,19 +1,43 @@
-function log(target, key){
-  console.log(`${key} se ha llamado`);
+// function log(target, key){
+//   console.log(`${key} se ha llamado`);
+// }
+
+// class Persona {
+//   private name: string;
+
+//   constructor(name: string){
+//     this.name = name;
+//   }
+
+//   @log
+//   sayMyName(){
+//     console.log(this.name);
+//   }
+// }
+
+// const persona: Persona = new Persona('Alan');
+// persona.sayMyName(); // 'Alan'
+
+function init(target){
+  return class extends target {
+    nombre = 'Alan';
+    apellido = 'Buscaglia';
+
+    sayMyName(){
+      return `${this.nombre} ${this.apellido}`;
+    }
+  }
 }
 
-class Persona {
-  private name: string;
 
-  constructor(name: string){
-    this.name = name;
+class P{
+  constructor(){
+
   }
-
-  @log
   sayMyName(){
-    console.log(this.name);
+
   }
 }
 
-const persona: Persona = new Persona('Alan');
-persona.sayMyName(); // 'Alan'
+const p: P = new P();
+console.log(p.sayMyName());
