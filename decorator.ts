14b5d617cb41dabf35d1435f3b4547768e5509b0
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-14 14:54:23 +0200

    decorator

---
+++ decorator.ts
@@ -0,0 +1,19 @@
+function log(target, key){
+  console.log(`${key} se ha llamado`);
+}
+
+class Persona {
+  private name: string;
+
+  constructor(name: string){
+    this.name = name;
+  }
+
+  @log
+  sayMyName(){
+    console.log(this.name);
+  }
+}
+
+const persona: Persona = new Persona('Alan');
+persona.sayMyName(); // 'Alan'
--- readme.md
+++ readme.md
@@ -6,3 +6,4 @@
 - Union types
 - Intersection types
 - Function type
+- Decorator
